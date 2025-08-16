1a3f0d69f911064cae32815660c0b7e6436c5d87
Nico <niconel@gmail.com>
2025-08-16 16:28:50 +0200

    try fix

--- apps/api/src/admin/client/client.module.ts
+++ apps/api/src/admin/client/client.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ClientController } from './client.controller';
 import { ClientService } from './client.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ClientController],
   providers: [ClientService, PrismaService],
   exports: [ClientService],
--- apps/api/src/specialist/specialist.module.ts
+++ apps/api/src/specialist/specialist.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { SpecialistService } from './specialist.service';
 import { SpecialistController } from './specialist.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [SpecialistController],
   providers: [SpecialistService, PrismaService],
   exports: [SpecialistService],

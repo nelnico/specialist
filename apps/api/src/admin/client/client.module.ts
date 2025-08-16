ef77e0b2cb40c7889ab7ad72b7d83f59201dc8b6
Nico <niconel@gmail.com>
2025-08-16 17:18:57 +0200

    see if all still fine

--- apps/api/src/admin/client/client.module.ts
+++ apps/api/src/admin/client/client.module.ts
@@ -1,13 +1,13 @@
-// import { Module } from '@nestjs/common';
-// import { ClientController } from './client.controller';
-// import { ClientService } from './client.service';
-// import { PrismaService } from 'src/prisma/prisma.service';
-// import { PrismaModule } from 'src/prisma/prisma.module';
+import { Module } from '@nestjs/common';
+import { ClientController } from './client.controller';
+import { ClientService } from './client.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from '../../prisma/prisma.module';
 
-// @Module({
-//   imports: [PrismaModule],
-//   controllers: [ClientController],
-//   providers: [ClientService, PrismaService],
-//   exports: [ClientService],
-// })
-// export class ClientModule {}
+@Module({
+  imports: [PrismaModule],
+  controllers: [ClientController],
+  providers: [ClientService, PrismaService],
+  exports: [ClientService],
+})
+export class ClientModule {}

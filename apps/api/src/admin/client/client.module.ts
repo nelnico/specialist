615988072331fde92c31a5231bb77ac9b8e57327
Nico <niconel@gmail.com>
2025-08-16 17:26:02 +0200

    try fix again

--- apps/api/src/admin/client/client.module.ts
+++ apps/api/src/admin/client/client.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ClientController } from './client.controller';
 import { ClientService } from './client.service';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../../prisma/prisma.service';
 import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
